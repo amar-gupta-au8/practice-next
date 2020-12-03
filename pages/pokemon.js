b76170839eb5d46681bd10d3c3711d437a8647e6
theamargupta <amar@lestransformations.com>
2020-12-03 21:49:00 +0530

    fix: link as small letters

--- pages/pokemon.js
+++ pages/pokemon.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Layout from '../components/Layout';
-import Link from 'next/Link';
+import Link from 'next/link';
 import axios from 'axios';
 
 const pokemon = ({ pokeman, id }) => {
